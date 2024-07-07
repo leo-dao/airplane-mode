1cea7de53608a200443f933389667cf716357885
Leo Dao <leotomdao@gmail.com>
2024-07-07 15:35:45 +0200

    Fixed time display issue and overzealous error catching

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -3,14 +3,6 @@ from frappe.website.website_generator import WebsiteGenerator
 from frappe import _
 
 class AirplaneFlight(WebsiteGenerator):
-    def validate(self):
-        if self.status not in ["Scheduled", "Completed", "Cancelled"]:
-            frappe.throw(_("Invalid status"))
 
     def on_submit(self):
         self.status = 'Completed'
-
-    def get_context(self, context):
-        context.published = self.published
-        if not self.published:
-            raise frappe.PageNotFoundError
