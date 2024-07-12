46eca7a3816541dd76ca906a9532f71a346d3aa8
Leo Dao <leotomdao@gmail.com>
2024-07-12 08:08:32 +0200

    Created available shops html file + small fixes

--- airplane_mode/airplane_mode/doctype/airport/airport.py
+++ airplane_mode/airplane_mode/doctype/airport/airport.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2024, Leo Dao and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Airport(Document):
-	pass
+	
+	def update_shop_count(airport_name):
+		shop_count = frappe.db.count('Shop', filters={'airport': airport_name})
+		frappe.db.set_value('Airport', airport_name, 'shop_count', shop_count)
+		frappe.db.commit()
